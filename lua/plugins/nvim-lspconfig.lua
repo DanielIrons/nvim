03fede7e71a1829b9ae98f2f6b0fdece6a10627e
danielirons <daniel.w.irons@gmail.com>
2024-01-03 09:44:59 -0800

    Initial commit

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,152 @@
+local on_attach = require("util.lsp").on_attach
+local diagnostic_signs = require("util.icons").diagnostic_signs
+
+local config = function()
+	require("neoconf").setup({})
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	local lspconfig = require("lspconfig")
+	local capabilities = cmp_nvim_lsp.default_capabilities()
+
+	for type, icon in pairs(diagnostic_signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	end
+
+	-- lua
+	lspconfig.lua_ls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		settings = { -- custom settings for lua
+			Lua = {
+				-- make the language server recognize "vim" global
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make language server aware of runtime files
+					library = {
+						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+						[vim.fn.stdpath("config") .. "/lua"] = true,
+					},
+				},
+			},
+		},
+	})
+
+	lspconfig.gopls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "go" },
+	})
+
+	lspconfig.rust_analyzer.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "rust" },
+		settings = {
+			["rust-analyzer"] = {
+				diagnostics = {
+					disabled = { "inactive-code" },
+				},
+			},
+		},
+	})
+
+	-- json
+	lspconfig.jsonls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "json", "jsonc" },
+	})
+
+	-- bash
+	lspconfig.bashls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "sh", "aliasrc" },
+	})
+
+	-- docker
+	lspconfig.dockerls.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+	})
+
+	-- C/C++
+	lspconfig.clangd.setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		cmd = {
+			"clangd",
+			"--offset-encoding=utf-16",
+		},
+	})
+
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+	local eslint = require("efmls-configs.linters.eslint")
+	local prettier_d = require("efmls-configs.formatters.prettier_d")
+	local fixjson = require("efmls-configs.formatters.fixjson")
+	local shellcheck = require("efmls-configs.linters.shellcheck")
+	local shfmt = require("efmls-configs.formatters.shfmt")
+	local hadolint = require("efmls-configs.linters.hadolint")
+	local cpplint = require("efmls-configs.linters.cpplint")
+	local clangformat = require("efmls-configs.formatters.clang_format")
+	local revive = require("efmls-configs.linters.go_revive")
+	local gofumpt = require("efmls-configs.formatters.gofumpt")
+	local goimports = require("efmls-configs.formatters.goimports")
+
+	-- configure efm server
+	lspconfig.efm.setup({
+		filetypes = {
+			"lua",
+			"json",
+			"jsonc",
+			"sh",
+			"markdown",
+			"docker",
+			"html",
+			"css",
+			"c",
+			"cpp",
+			"go",
+		},
+		init_options = {
+			documentFormatting = true,
+			documentRangeFormatting = true,
+			hover = true,
+			documentSymbol = true,
+			codeAction = true,
+			completion = true,
+		},
+		settings = {
+			languages = {
+				go = { revive, gofumpt, goimports },
+				lua = { luacheck, stylua },
+				json = { eslint, fixjson },
+				jsonc = { eslint, fixjson },
+				sh = { shellcheck, shfmt },
+				markdown = { prettier_d },
+				docker = { hadolint, prettier_d },
+				html = { prettier_d },
+				css = { prettier_d },
+				c = { clangformat, cpplint },
+				cpp = { clangformat, cpplint },
+			},
+		},
+	})
+end
+
+return {
+	"neovim/nvim-lspconfig",
+	config = config,
+	lazy = false,
+	dependencies = {
+		"windwp/nvim-autopairs",
+		"williamboman/mason.nvim",
+		"creativenull/efmls-configs-nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+	},
+}
